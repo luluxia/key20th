4fead677043a3ee4650ed4a7c03115cbd0de2d50
luluxia <306924625@qq.com>
2021-02-01 17:02:06 +0800

    更新动画

--- src/App.jsx
+++ src/App.jsx
@@ -259,10 +259,36 @@ function SwitchPlanting(props) {
 }
 // 生日
 function Birth() {
+  const { id } = useContext(Context)
+  const birthRef = useRef({
+    m: 1,
+    d: 1
+  })
+  const domRef = useRef([])
+  useEffect(() => {
+    anime({
+      targets: birthRef.current,
+      m: data[id].month != 13 ? data[id].month : 0,
+      d: data[id].day != 32 ? data[id].day : 0,
+      round: 1,
+      duration: 600,
+      easing: "linear",
+      update: () => {
+        domRef.current[0].innerHTML = birthRef.current.m
+        domRef.current[1].innerHTML = birthRef.current.d
+      }
+    })
+  }, id)
   return (
-    <div style={{transform: `translate(-12rem, 0px)`}} className="info-birth">
+    <div style={{
+        transform: `translate(-${data[id].name[0].length * 3.375 + 1}rem, 0px)`,
+        opacity: data[id].month == 13 ? 0 : 1
+      }} className="info-birth">
       <img src="img/other/cake.svg" alt=""/>
-      <span>12月24日</span>
+      <span>
+        <span ref={e => {domRef.current[0] = e}}></span>月
+        <span ref={e => {domRef.current[1] = e}}></span>日
+      </span>
     </div>
   )
 }
@@ -274,32 +300,112 @@ function Name() {
     setName(data[id].name[0].split('').map(e => [e]))
   }, id)
   return (
-    <div className="info-name">
-      <h1>
-        {
-          name.map((item, index) => (
-            <span key={index} className="name-zh">
-              {item[0]}
-              {
-                item[1] &&
-                <span
-                  style={{textAlignLast: item[1].length == 1 ? 'center' : 'justify'}}
-                  className="name-kana">{item[1]}
-                </span>
-              }
-            </span>
-          ))
-        }
-      </h1>
-    </div>
+    <TransitionGroup className="info-name">
+      {
+        id.map(id => (
+          <Transition key={id} timeout={1500}>
+            { status => <NameTrans key={id} name={name} status={status}/> }
+          </Transition>
+        ))
+      }
+    </TransitionGroup>
+  )
+}
+// 姓名切换
+function NameTrans(props) {
+  const ref = useRef(null)
+  useEffect(() => {
+    if (props.status == 'entering') {
+      anime({
+        targets: ref.current.children,
+        translateX: [40,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 30 * i
+      })
+    } else if (props.status == 'exiting') {
+      anime({
+        targets: ref.current.children,
+        translateX: [0,-30],
+        opacity: [1,0],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 30 * i
+      })
+    }
+  }, [props.status])
+  return (
+    <h1 ref={ref}>
+      {
+        props.name.map((item, index) => (
+          <span key={index} className="name-zh">
+            {item[0]}
+            {
+              item[1] &&
+              <span
+                style={{textAlignLast: item[1].length == 1 ? 'center' : 'justify'}}
+                className="name-kana">{item[1]}
+              </span>
+            }
+          </span>
+        ))
+      }
+    </h1>
   )
 }
 // 罗马名
 function Romaji() {
+  const { id } = useContext(Context)
   return (
-    <div className="info-romaji">
-      <p>Furukawa Nagisa</p>
-    </div>
+    <TransitionGroup className="info-romaji">
+      {
+        id.map(id => (
+          <Transition key={id} timeout={1500}>
+            { status => <RomajiTrans id={id} status={status}/> }
+          </Transition>
+        ))
+      }
+    </TransitionGroup>
+  )
+}
+function RomajiTrans(props) {
+  const ref = useRef(null)
+  useEffect(() => {
+    if (props.status == 'entering') {
+      anime({
+        targets: ref.current.children,
+        translateY: ["1.1em", 0],
+        translateX: ["0.55em", 0],
+        translateZ: 0,
+        rotateZ: [180, 0],
+        opacity: [0, 1],
+        duration: 750,
+        easing: "easeOutExpo",
+        delay: (el, i) => 50 * i
+      })
+      anime({
+        targets: ref.current,
+        opacity: [0, 0.1],
+        duration: 1000,
+        easing: "easeOutExpo"
+      })
+    } else if (props.status == 'exiting') {
+      anime({
+        targets: ref.current,
+        opacity: [0.1, 0],
+        duration: 1000,
+        easing: "easeOutExpo"
+      })
+    }
+  }, [props.status])
+  return (
+    <p ref={ref}>
+      {
+        data[props.id].name[3]?.split('').map(item => (<span>{item}</span>))
+      }
+    </p>
   )
 }
 // 介绍
@@ -308,7 +414,7 @@ function Intro() {
   return (
     <div className="info-intro">
       <p>
-        古河渚性格乐观温柔，想创立演剧部但是一直缺乏自信。在樱花飞舞的日子里与男主角冈崎朋也相遇，在动漫的渚线中最终创立演剧部成为部长，并与朋也交往。在AFTER STORY中与朋也结婚，改名冈崎渚，生下冈崎汐。患有一种不知名的病，经常休学在家，所以虽然与朋也同一个年级但是却比朋也大一岁。最喜欢的歌曲是《团子大家族》，招募演剧部部员的海报画的就是团子大家族。
+        { data[id]?.intro ?? "(｡･∀･)ﾉﾞ嗨，欢迎来到Key Character Birthday Timeline（Key20th Version），本站正并且将长期处于建设中（咕咕咕~），如果你看到了这段话，那说明这位角色的介绍还没有添加，如果你想要为角色补充更多信息、或者发现了信息错误，亦或是想要添加一些新角色的话，欢迎前往GIthub参与补充！" }
       </p>
       <ul className="info-list">
         <li><span>CV</span>中原麻衣</li>
